203f099db50cb10878e0823dc746ee037c1353b3
Hamza Shakeel <47722357+hamzanufc10@users.noreply.github.com>
2019-12-01 20:35:12 +0530

    Update detector.py

--- detector.py
+++ detector.py
@@ -1,15 +1,21 @@
 import numpy as np
 import cv2
 import webbrowser
+import os
 
 faceDetect = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 cap=cv2.VideoCapture(0)
 rec=cv2.face.LBPHFaceRecognizer_create()
 rec.read('trainner\\trainner.yml')
 id=0
+#cv2.FONT_HERSHEY_SIMPLEX
 fontface = cv2.FONT_HERSHEY_SIMPLEX
-fontscale = 1.5
+fontscale = 2
+thickness = 2
 fontcolor = (5000 ,0 ,0)
+c=0
+d=0
+e=0
 while(True):
     ret, img = cap.read()
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -19,13 +25,34 @@ while(True):
         Id, conf = rec.predict(gray[y:y + h, x:x + w])
         if (Id == 1):
             Id = "Hamza"
+            c+=1
+            if c==1:
+                webbrowser.open('https://www.instagram.com/hamza.nufc10/')
+                break
+            elif c==2:
+                break
+                c=0
         elif(Id==2):
             Id="Messi"
+            e += 1
+            if e == 1:
+                webbrowser.open('https://www.instagram.com/leomessi/')
+                break
+            elif e == 2:
+                break
+                e = 0
         elif(Id==3):
             Id="Ronaldo"
+            d += 1
+            if d == 1:
+                webbrowser.open('https://www.instagram.com/cristiano/')
+                break
+            elif d == 2:
+                break
+                d=0
         else:
             Id="unknown"
-        cv2.putText(img, str(Id), (x,y+h), fontface, fontscale, fontcolor)
+        cv2.putText(img, str(Id), (x,y+h), fontface,fontscale, fontcolor,thickness)
 
     cv2.imshow('frame',img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
